589f3f78a92f393ac8f6caa58ce9e6c848d404b7
fleish80 <Fleish.D@gmail.com>
2015-10-20 23:33:46 +0300

    Initial

---
+++ AngularGoogleMap/App.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="D:\Dev\AngularGoogleMap\AngularGoogleMap\Scripts/angular.js" />
+
+var googleMapApp = angular.module('googleMapApp', []);
+
+googleMapApp.controller('googleMapCtrl', ['$scope', function ($scope) {
+    $scope.title = 'Some message';
+}]);
