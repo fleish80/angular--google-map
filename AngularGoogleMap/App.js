58001717265541e5f3dc56caa2b09b07d513c3e1
fleish80 <Fleish.D@gmail.com>
2015-10-21 22:42:36 +0300

    Map is visible

--- AngularGoogleMap/App.js
+++ AngularGoogleMap/App.js
@@ -1,7 +1,9 @@
 ﻿/// <reference path="D:\Dev\AngularGoogleMap\AngularGoogleMap\Scripts/angular.js" />
 
-var googleMapApp = angular.module('googleMapApp', []);
+var googleMapApp = angular.module('googleMapApp', ['uiGmapgoogle-maps']);
+
 
 googleMapApp.controller('googleMapCtrl', ['$scope', function ($scope) {
     $scope.title = 'Some message';
+    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
 }]);
